ee8f3cdbfcd4c5a9d431d7564e6607cc79ffaaa4
acrylic-style <me@acrylicstyle.xyz>
2020-03-28 02:57:49 +0900

    add files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # discord-api
-An open API to get Discord users (discord.js + expressjs)
+An simple open API to get Discord users (discord.js + expressjs)
+
+Open a PR if you have any feature requests!
---
+++ index.js
@@ -0,0 +1,64 @@
+const app = require('express')()
+const Discord = require('discord.js')
+const { LoggerFactory } = require('logger.js')
+const logger = {
+  discord: LoggerFactory.getLogger('discord', 'yellow'),
+  express: LoggerFactory.getLogger('express', 'green'),
+}
+const env = require('dotenv-safe').config().parsed
+const client = new Discord.Client()
+
+const toJSON = user => ({
+  discriminator: user.discriminator,
+  bot: user.bot,
+  tag: user.tag,
+  defaultAvatarURL: user.defaultAvatarURL,
+  avatarURL: user.avatarURL,
+  avatar: user.avatar,
+  createdAt: user.createdAt,
+  createdTimestamp: user.createdTimestamp,
+  id: user.id,
+  locale: user.locale,
+  partial: user.partial,
+  system: user.system,
+  username: user.username,
+  presence: {
+    activities: user.presence.activities.map(a => ({
+      createdAt: a.createdAt,
+      applicationID: a.applicationID,
+      createdTimestamp: a.createdTimestamp,
+      details: a.details,
+      emoji: a.emoji,
+      name: a.name,
+      flags: a.flags,
+      party: a.party,
+      state: a.state,
+      timestamps: a.timestamps,
+      type: a.type,
+      url: a.url,
+    })),
+    clientStatus: user.presence.clientStatus,
+    status: user.presence.status,
+    userID: user.presence.userID,
+  },
+})
+
+app.get('/users/:user_id', async (req, res) => {
+  const user_id = req.params.user_id
+  const user = client.users.cache.has(user_id) ? client.users.cache.get(user_id) : await client.users.fetch(user_id).catch(() => {
+    res.status(404).json({ status: 404, message: 'User could not found by specified ID.' })
+    return null
+  })
+  if (!user) return
+  res.status(200).json(toJSON(user))
+})
+
+app.listen(env.PORT, () => {
+  logger.express.info('Express is ready.')
+})
+
+client.on('ready', () => {
+  logger.discord.info('Discord is ready.')
+})
+
+client.login(env.TOKEN)
